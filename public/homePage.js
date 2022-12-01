721f679346e91b0441460e0f9548e25332a5c2fc
Artem <temas1994@mail.ru>
2022-12-01 15:21:56 +0300

    like-sale-btn

---
+++ public/homePage.js
@@ -0,0 +1,31 @@
+document.querySelector('#products-container').addEventListener('click', async (event) => {
+  event.preventDefault();
+  if (event.target.classList.contains('like-btn')) {
+    const likeBtn = event.target;
+    const card = likeBtn.closest('.card');
+    const id = Number(card.dataset.id);
+    const response = await fetch('/api/card', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id }),
+    });
+    const result = await response.json();
+    if (result.success) alert('товар добавлен в избранное');
+    else alert('товар уже в избранном');
+  } else if (event.target.classList.contains('sale-btn')) {
+    const saleBtn = event.target;
+    const card = saleBtn.closest('.card');
+    const id = Number(card.dataset.id);
+    const response = await fetch('/api/card/sale', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id }),
+    });
+    const result = await response.json();
+    if (result.success) alert('товар добавлен в корзину');
+  }
+});
